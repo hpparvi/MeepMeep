d699fd92ab97b9d01e3108afaf78ff25256f5cf9
Hannu Parviainen <hannu@iac.es>
2022-11-18 15:28:48 +0000

    Fixed a numerical bug in true_anomaly_o5v.

--- snappyko/xyz5.py
+++ snappyko/xyz5.py
@@ -267,7 +267,9 @@ def true_anomaly_o5v(times, t0, p, ex, ey, ez, w, dt, points, coeffs):
             vx, vy, vz = vxyz_o5s(times[i], t0, p, dt, points, coeffs)
             edp = (x*ex + y*ey + z*ez) / sqrt((x**2 + y**2 + z**2) * nes)
 
-            if (x*vx + y*vy + z*vz) > 0.0:
+            if (edp+1.0) < 1e-8:
+                f[i] = pi
+            elif (x*vx + y*vy + z*vz) > 0.0:
                 f[i] = arccos(edp)
             else:
                 f[i] = 2.0*pi - arccos(edp)
