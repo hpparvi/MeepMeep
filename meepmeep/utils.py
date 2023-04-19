9439092b1c74fb80e74431006c7b85f9c054c067
Hannu Parviainen <hannu@iac.es>
2023-04-19 12:39:40 +0100

    Added some utility functions.

--- meepmeep/utils.py
+++ meepmeep/utils.py
@@ -16,7 +16,7 @@
 
 
 from numba import njit
-from numpy import pi, arctan2, sqrt, sin, cos, arccos, mod, copysign, sign, array
+from numpy import pi, arctan2, sqrt, sin, cos, arccos, mod, copysign, sign, array, arcsin
 from scipy.constants import G
 
 HALF_PI = 0.5*pi
@@ -136,3 +136,42 @@ def z_from_ta_v(Ta, a, i, e, w):
     z  = a*(1.0-e**2)/(1.0+e*cos(Ta)) * sqrt(1.0 - sin(w+Ta)**2 * sin(i)**2)
     z *= sign(1.0, sin(w+Ta))
     return z
+
+
+@njit
+def impact_parameter(a, i):
+    return a * cos(i)
+
+
+@njit
+def impact_parameter_ec(a, i, e, w, tr_sign):
+    return a * cos(i) * ((1.-e**2) / (1.+tr_sign*e*sin(w)))
+
+@njit
+def d_from_pkaiews(p, k, a, i, e, w, tr_sign, kind=14):
+    """Transit duration (T14 or T23) from p, k, a, i, e, w, and the transit sign.
+
+    Calculates the transit duration (T14) from the orbital period, planet-star radius ratio, scaled semi-major axis,
+    orbital inclination, eccentricity, argument of periastron, and the sign of the transit (transit:1, eclipse: -1).
+
+     Parameters
+     ----------
+
+       p  : orbital period         [d]
+       k  : radius ratio           [R_Star]
+       a  : scaled semi-major axis [R_star]
+       i  : orbital inclination    [rad]
+       e  : eccentricity           [-]
+       w  : argument of periastron [rad]
+       tr_sign : transit sign, 1 for a transit, -1 for an eclipse
+       kind: either 14 for full transit duration or 23 for total transit duration
+
+     Returns
+     -------
+
+       d  : transit duration T14  [d]
+     """
+    b  = impact_parameter_ec(a, i, e, w, tr_sign)
+    ae = sqrt(1.-e**2)/(1.+tr_sign*e*sin(w))
+    ds = 1. if kind == 14 else -1.
+    return p/pi  * arcsin(sqrt((1.+ds*k)**2-b**2)/(a*sin(i))) * ae
