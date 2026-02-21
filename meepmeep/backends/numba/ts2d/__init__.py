fc761a12ef1b1225fd823195b3fbce920246b65a
Hannu Parviainen <hpparvi@gmail.com>
2026-02-21 17:06:18 +0000

    Fixed numba backend __init__.py

--- meepmeep/backends/numba/ts2d/__init__.py
+++ meepmeep/backends/numba/ts2d/__init__.py
@@ -1,9 +1,8 @@
 """Taylor series 2D (sky-projected) orbit computations."""
-from . import position, positiond, derivatives, par_direct, par_fitting
+from . import position, positiond
 from .position import solve_xy_p5, xy_t15, xy_t15c, pd_t15, pd_t15c
-from .positiond import solve_xy_p5_d, xy_t15_d, pd_t15_d
-from .derivatives import pd_with_derivatives_v, pd_with_derivatives_s
+from .positiond import solve_xy_p5_d, xy_t15_d, xy_t15c_d, pd_t15_d, pd_t15c_d
 
-__all__ = ['position', 'positiond', 'derivatives', 'par_direct', 'par_fitting',
-           'solve_xy_p5', 'solve_xy_p5_d', 'xy_t15', 'xy_t15_d', 'xy_t15c', 'pd_t15', 'pd_t15_d', 'pd_t15c',
-           'pd_with_derivatives_v', 'pd_with_derivatives_s']
+__all__ = ['position', 'positiond',
+           'solve_xy_p5', 'solve_xy_p5_d', 'xy_t15', 'xy_t15_d', 'xy_t15c_d', 'xy_t15c',
+           'pd_t15', 'pd_t15_d', 'pd_t15c', 'pd_t15c_d']
