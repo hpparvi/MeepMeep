0c3cc1a9dca63bdd773c4cfbeebff0b5d88a2233
Hannu Parviainen <hpparvi@gmail.com>
2026-02-21 17:03:09 +0000

    Added centered numba pd methods with gradients.

---
+++ meepmeep/backends/jax/ts2d/__init__.py
@@ -0,0 +1,1 @@
+from .positiond import solve_xy_p5, solve_xy_p5_d, xy_t15_d, pd_t15_d
--- meepmeep/backends/numba/ts2d/positiond.py
+++ meepmeep/backends/numba/ts2d/positiond.py
@@ -304,14 +304,49 @@ def solve_xy_p5_d(phase, p, a, i, e, w):
 
     return cf, dcf
 
-
 @njit(fastmath=True)
-def xy_t15_d(tc, t0, p, c, dc):
+def xy_t15c_d(t, c, dc):
     """Calculate planet's (x, y) position and parameter derivatives using Taylor series.
 
     Parameters
     ----------
-    tc : float
+    t : float
+        The current time.
+    c : ndarray (2, 5)
+        Position Taylor coefficients from solve_xy_p5.
+    dc : ndarray (6, 2, 5)
+        Parameter derivative coefficients from solve_xy_p5_d.
+
+    Returns
+    -------
+    px : float
+        Sky-plane x position.
+    py : float
+        Sky-plane y position.
+    dpx : ndarray (6,)
+        Derivatives of px w.r.t. (phase, p, a, i, e, w).
+    dpy : ndarray (6,)
+        Derivatives of py w.r.t. (phase, p, a, i, e, w).
+    """
+    px = c[0, 0] + t * (c[0, 1] + t * (c[0, 2] + t * (c[0, 3] + t * c[0, 4])))
+    py = c[1, 0] + t * (c[1, 1] + t * (c[1, 2] + t * (c[1, 3] + t * c[1, 4])))
+
+    dpx = zeros(6)
+    dpy = zeros(6)
+    for k in range(6):
+        dpx[k] = dc[k, 0, 0] + t * (dc[k, 0, 1] + t * (dc[k, 0, 2] + t * (dc[k, 0, 3] + t * dc[k, 0, 4])))
+        dpy[k] = dc[k, 1, 0] + t * (dc[k, 1, 1] + t * (dc[k, 1, 2] + t * (dc[k, 1, 3] + t * dc[k, 1, 4])))
+
+    return px, py, dpx, dpy
+
+
+@njit(fastmath=True)
+def xy_t15_d(t, t0, p, c, dc):
+    """Calculate planet's (x, y) position and parameter derivatives using Taylor series.
+
+    Parameters
+    ----------
+    t : float
         The current time.
     t0 : float
         The Taylor series expansion time.
@@ -333,19 +368,36 @@ def xy_t15_d(tc, t0, p, c, dc):
     dpy : ndarray (6,)
         Derivatives of py w.r.t. (phase, p, a, i, e, w).
     """
-    epoch = floor((tc - t0 + 0.5 * p) / p)
-    t = tc - (t0 + epoch * p)
+    epoch = floor((t - t0 + 0.5 * p) / p)
+    return xy_t15c_d(tc - (t0 + epoch * p), c, dc)
 
-    px = c[0, 0] + t * (c[0, 1] + t * (c[0, 2] + t * (c[0, 3] + t * c[0, 4])))
-    py = c[1, 0] + t * (c[1, 1] + t * (c[1, 2] + t * (c[1, 3] + t * c[1, 4])))
 
-    dpx = zeros(6)
-    dpy = zeros(6)
+@njit(fastmath=True)
+def pd_t15c_d(t, c, dc):
+    """Calculate projected planet-star distance and its parameter derivatives.
+
+    Parameters
+    ----------
+    tc : float
+        The current time.
+    c : ndarray (2, 5)
+        Position Taylor coefficients from solve_xy_p5.
+    dc : ndarray (6, 2, 5)
+        Parameter derivative coefficients from solve_xy_p5_d.
+
+    Returns
+    -------
+    d : float
+        Projected planet-star distance.
+    dd : ndarray (6,)
+        Derivatives of d w.r.t. (phase, p, a, i, e, w).
+    """
+    px, py, dpx, dpy = xy_t15c_d(t, c, dc)
+    d = sqrt(px ** 2 + py ** 2)
+    dd = zeros(6)
     for k in range(6):
-        dpx[k] = dc[k, 0, 0] + t * (dc[k, 0, 1] + t * (dc[k, 0, 2] + t * (dc[k, 0, 3] + t * dc[k, 0, 4])))
-        dpy[k] = dc[k, 1, 0] + t * (dc[k, 1, 1] + t * (dc[k, 1, 2] + t * (dc[k, 1, 3] + t * dc[k, 1, 4])))
-
-    return px, py, dpx, dpy
+        dd[k] = (px * dpx[k] + py * dpy[k]) / d
+    return d, dd
 
 
 @njit(fastmath=True)
@@ -372,9 +424,5 @@ def pd_t15_d(tc, t0, p, c, dc):
     dd : ndarray (6,)
         Derivatives of d w.r.t. (phase, p, a, i, e, w).
     """
-    px, py, dpx, dpy = xy_t15_d(tc, t0, p, c, dc)
-    d = sqrt(px ** 2 + py ** 2)
-    dd = zeros(6)
-    for k in range(6):
-        dd[k] = (px * dpx[k] + py * dpy[k]) / d
-    return d, dd
+    epoch = floor((t - t0 + 0.5 * p) / p)
+    return pd_t15c_d(tc - (t0 + epoch * p), c, dc)
