58d479f103015afdd2867dd1888d4c1e6713f7c4
Hannu Parviainen <hpparvi@gmail.com>
2026-03-23 21:39:49 +0000

    Moved the 3D TS solve functions to their own modules.

--- meepmeep/backends/numba/ts3d/__init__.py
+++ meepmeep/backends/numba/ts3d/__init__.py
@@ -1,17 +1,16 @@
 """Taylor series 3D orbit computations."""
 from . import position, positiond, extended
-from .position import (solve_xyz_p5,
-                        xyz_t15, xyz_t15c, xyzd_t15c,
-                        pd_t15, pd_t15c,
-                        z_t15, z_t15c,
-                        vxyz_t15c, vz_t15, vz_t15c,
-                        find_contact_point, bounding_box)
-from .positiond import (solve_xyz_p5_d,
-                         xyz_t15_d, xyz_t15c_d,
-                         pd_t15_d, pd_t15c_d)
+from .position import (xyz_t15, xyz_t15c, xyzd_t15c,
+                       pd_t15, pd_t15c,
+                       z_t15, z_t15c,
+                       vxyz_t15c, vz_t15, vz_t15c,
+                       find_contact_point, bounding_box)
+from .solve import solve
+from .positiond import (xyz_t15_d, xyz_t15c_d,
+                        pd_t15_d, pd_t15c_d)
+from .solved import solve_d
 
-__all__ = ['position', 'positiond', 'extended',
-           'solve_xyz_p5', 'solve_xyz_p5_d',
+__all__ = ['position', 'positiond', 'extended', 'solve', 'solve_d',
            'xyz_t15', 'xyz_t15_d', 'xyz_t15c', 'xyz_t15c_d', 'xyzd_t15c',
            'pd_t15', 'pd_t15_d', 'pd_t15c', 'pd_t15c_d',
            'z_t15', 'z_t15c',
